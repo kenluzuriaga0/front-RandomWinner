80baa99cac694010e93254ef9402c485491c9be1
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2021-06-27 16:16:59 -0500

    add animation in randomNumber

--- code.js
+++ code.js
@@ -1,5 +1,25 @@
-console.log("ostia");
-var x = document.getElementsByClassName("title").innerHTML()
-console.log(x);
 
-document.write("ostia")
+
+var animacionNum = document.querySelector('.numero');
+
+document.querySelector('input').addEventListener('click', function () {
+  anime({
+    targets: animacionNum,
+    innerHTML: [0, 200],
+    easing: 'linear',
+    round: 1,
+    duration: 900,
+    update: function(){
+      let numerito = (Math.floor(Math.random() * 120)) + 1;
+      animacionNum.innerHTML = numerito;
+      console.log(numerito)
+    }
+     
+
+  });
+});
+
+
+function getNumeroWinner(animacionNum) {
+
+}
